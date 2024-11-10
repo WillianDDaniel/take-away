cfd2c1299a5e68101bb1c0fdb086ebd1b22015db
WillianDDaniel <williandeivitidaniel@live.com>
2024-11-09 21:02:32 -0300

    feat: added informations about portions to beverage index page

--- spec/system/beverages/index_beverage_page_spec.rb
+++ spec/system/beverages/index_beverage_page_spec.rb
@@ -60,37 +60,88 @@ describe 'Index Beverage Page' do
 
   it 'should have a list of beverages' do
     user = User.create!(
-      email: 'johndoe@example.com', name: 'John', last_name: 'Doe',
+      email: 'johndoes@example.com', name: 'John', last_name: 'Doe',
       password: 'password12345', document_number: CPF.generate
     )
 
-    Restaurant.create!(
-      brand_name: 'Restaurante Teste', corporate_name: 'Teste', email: 'johndoe@example.com',
+    restaurant = Restaurant.create!(
+      brand_name: 'Restaurante Teste', corporate_name: 'Teste', email: 'johndoes@example.com',
       phone: '51993831972', address: 'Rua Teste',
       doc_number: CNPJ.generate, user: user
     )
 
+    beverage_01 = Beverage.create!(
+      name: 'Bebida Teste', description: 'Descrição Teste',
+      alcoholic: true, restaurant: restaurant
+    )
+
+    Portion.create!(
+      description: 'Lata 350ml',
+      price: 10, portionable: beverage_01
+    )
+
+    Portion.create!(
+      description: 'Garafa 500ml',
+      price: 20, portionable: beverage_01
+    )
+
+    beverage_02 = Beverage.create!(
+      name: 'Outro Teste', description: 'Outra Descrição',
+      alcoholic: false, restaurant: restaurant
+    )
+
+    Portion.create!(
+      description: 'Lata 350ml',
+      price: 25, portionable: beverage_02
+    )
+
+    Portion.create!(
+      description: 'Garafa 2L',
+      price: 35, portionable: beverage_02
+    )
+
     login_as(user)
+    visit beverages_path
 
-    Beverage.create!(
-      name: 'Cerveja', description: 'Cerveja lata',
-      calories: 200, alcoholic: true,
-      restaurant: Restaurant.last
+    expect(page).to have_content('Bebida Teste')
+    expect(page).to have_content('Descrição Teste')
+    expect(page).to have_content('Lata 350ml')
+    expect(page).to have_content('R$ 10,00')
+    expect(page).to have_content('Garafa 500ml')
+    expect(page).to have_content('R$ 20,00')
+
+    expect(page).to have_content('Outro Teste')
+    expect(page).to have_content('Outra Descrição')
+    expect(page).to have_content('Lata 350ml')
+    expect(page).to have_content('R$ 25,00')
+    expect(page).to have_content('Garafa 2L')
+    expect(page).to have_content('R$ 35,00')
+  end
+
+  it 'should show a message if the beverage have no portion' do
+    user = User.create!(
+      email: 'johndoes@example.com', name: 'John', last_name: 'Doe',
+      password: 'password12345', document_number: CPF.generate
+    )
+
+    restaurant = Restaurant.create!(
+      brand_name: 'Restaurante Teste', corporate_name: 'Teste', email: 'johndoes@example.com',
+      phone: '51993831972', address: 'Rua Teste',
+      doc_number: CNPJ.generate, user: user
     )
 
     Beverage.create!(
-      name: 'Refrigerante', description: 'Refrigerante lata',
-      calories: 200, alcoholic: true,
-      restaurant: Restaurant.last
+      name: 'Bebida Teste', description: 'Descrição Teste',
+      alcoholic: false, restaurant: restaurant
     )
+
+    login_as(user)
 
     visit beverages_path
 
-    expect(page).to have_link 'Cerveja'
-    expect(page).to have_content('Cerveja lata')
-
-    expect(page).to have_link 'Refrigerante'
-    expect(page).to have_content('Refrigerante lata')
+    expect(page).to have_content('Nenhuma porção cadastrada para essa bebida.')
+    expect(page).to have_content('Clique no botão abaixo para adicionar porções.')
+    expect(page).to have_link('Cadastrar porção')
   end
 
   it 'beverages must show active or paused status' do
